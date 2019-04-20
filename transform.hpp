f0399d6dff12b107c7b7ddd1464dd862dd616f24
Navin Mohan <navinmohan81@gmail.com>
2019-04-20 19:44:00 +0530

    parallelized the for loops

--- transform.hpp
+++ transform.hpp
@@ -4,10 +4,14 @@
 namespace transform{
     template<class type, class container>
     container map(container arr, type func(type)){
-        auto new_arr = container();
-        for(auto it=arr.begin(); it!=arr.end(); it++){
-            auto new_ele = func(*it);
-            new_arr.insert(new_arr.end(), new_ele);
+        auto new_arr = container(arr.size());
+        size_t len = arr.size();
+        auto it = arr.begin();
+        auto new_it = new_arr.begin();
+        #pragma omp parallel for schedule(static)
+        for(size_t i = 0; i < len; i++){
+            auto new_ele = func(*(it+i));
+            *(new_it+i) = new_ele;
         }
         return new_arr;
     }
@@ -15,17 +19,25 @@ namespace transform{
     template<class type, class container>
     container filter(container arr, bool func(type)){
         auto new_arr = container();
-        for(auto it=arr.begin(); it!=arr.end(); it++){
-            if(func(*it))
-                new_arr.insert(new_arr.end(), *it);
+        size_t len = arr.size();
+        auto it = arr.begin();
+        #pragma omp parallel for schedule(static)
+        for(size_t i = 0; i < len; i++){
+            if(func(*it)){
+                #pragma omp critical                
+                {new_arr.insert(new_arr.end(), *(it+i));}
+            }
         }
         return new_arr;
     }
 
     template<class type, class container>
     type reduce(container arr, type func(type), type initial){
-        for(auto it=arr.begin(); it!=arr.end(); it++){
-            initial += func(*it);
+        size_t len = arr.size();
+        auto it = arr.begin();
+        #pragma omp parallel for schedule(static) reduction(+:initial)
+        for(size_t i = 0; i < len; i++){
+            initial += func(*(it+i));
         }
         return initial;
     }
